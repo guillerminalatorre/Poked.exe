2954abb92ac4f8dc991ad0ad443614871e37ff86
matiara <49988089+matiara@users.noreply.github.com>
2019-06-06 11:55:18 -0300

    Update IVolador.java

--- Pokedexa/src/Pokemon/IVolador.java
+++ Pokedexa/src/Pokemon/IVolador.java
@@ -1,6 +1,6 @@
 package Pokemon;
 
 public interface IVolador {
-  public static final String tipo = "Volador";
+	public static final String tipo = "Volador";
 	public static final int nivel = 7;
 }
