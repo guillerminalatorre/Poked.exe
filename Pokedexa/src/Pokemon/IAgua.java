b7be03b265ff7efe2e4ef005d462442f120dd805
ILopez2 <48885606+ILopez2@users.noreply.github.com>
2019-06-06 13:43:27 -0300

    06/06 Pokemon
    Correccion de nombres atributos.

--- Pokedexa/src/Pokemon/IAgua.java
+++ Pokedexa/src/Pokemon/IAgua.java
@@ -1,6 +1,6 @@
 package Pokemon;
 
 public interface IAgua {
-	public static final String tipo = "Agua";
-	public static final int nivel = 8;
+	public static final String tipoAgua = "Agua";
+	public static final int nivelAgua = 8;
 }
