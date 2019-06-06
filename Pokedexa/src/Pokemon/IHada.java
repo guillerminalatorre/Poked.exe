5b704065644232623e44d9830c39e6db4fb59a7e
ILopez2 <48885606+ILopez2@users.noreply.github.com>
2019-06-06 13:46:48 -0300

    06/06 Pokemon
    Correccion de nombres atributos.

--- Pokedexa/src/Pokemon/IHada.java
+++ Pokedexa/src/Pokemon/IHada.java
@@ -1,6 +1,6 @@
 package Pokemon;
 
 public interface IHada {
-  	public static final String tipo = "Hada";
-	public static final int nivel = 5;
+  	public static final String tipoHada = "Hada";
+	public static final int nivelHada = 5;
 }
