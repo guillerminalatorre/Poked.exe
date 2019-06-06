cfabf10a411f2d7e1ecd68d7e2cdd6b0ecc1c92d
matiara <49988089+matiara@users.noreply.github.com>
2019-06-06 11:59:01 -0300

    Update IFuego.java

--- Pokedexa/src/Pokemon/IFuego.java
+++ Pokedexa/src/Pokemon/IFuego.java
@@ -1,6 +1,6 @@
 package Pokemon;
 
 public interface IFuego {
-  	    public static final String tipo = "Fuego";
+        public static final String tipo = "Fuego";
         public static final int nivel = 8;
 }
