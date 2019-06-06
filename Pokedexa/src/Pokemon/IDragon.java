eb29d0216a89eaedb5f944e2ed84b84a80afc9de
ILopez2 <48885606+ILopez2@users.noreply.github.com>
2019-06-06 13:44:30 -0300

    06/06 Pokemon
    Correccion de nombres atributos.

--- Pokedexa/src/Pokemon/IDragon.java
+++ Pokedexa/src/Pokemon/IDragon.java
@@ -1,6 +1,6 @@
 package Pokemon;
 
 public interface IDragon {
-	public static final String tipo= "Dragon";
-	public static final int nivel= 20;
+	public static final String tipoDragon= "Dragon";
+	public static final int nivelDrago= 20;
 }
