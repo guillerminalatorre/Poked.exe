395665bc46fc3c6721a82cf85a6d9273a63e419c
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 20:52:17 -0300

    10/06 Modificaciones en clase  Electrico_Volador

--- Pokedexa/src/Pokemon/Electrico_Volador.java
+++ Pokedexa/src/Pokemon/Electrico_Volador.java
@@ -1,5 +1,45 @@
 package Pokemon;
 
-public class Electrico_Volador implements IElectrico , IVolador{
+public class Electrico_Volador extends Pokemon implements IElectrico , IVolador{
+	
+	/**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Electrico_Volador (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoElectrico + tipoVolador, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Electrico_Volador (int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoElectrico + tipoVolador, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelElectrico+nivelVolador+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
+
+
+
