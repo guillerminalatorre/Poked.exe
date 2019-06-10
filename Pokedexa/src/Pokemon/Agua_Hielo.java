c473d70e0c100883b2c365c4440d7c7d95ca6a7b
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 12:23:05 -0300

    10/06 Modificaciones en clase Agua_Hielo

--- Pokedexa/src/Pokemon/Agua_Hielo.java
+++ Pokedexa/src/Pokemon/Agua_Hielo.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
 public class Agua_Hielo implements IAgua , IHielo{
+	
+	/**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Agua_Hielo (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoAgua + tipoHielo, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Agua_Hielo(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoAgua + tipoHielo, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelAgua+nivelHielo+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
