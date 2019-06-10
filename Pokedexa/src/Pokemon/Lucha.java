9e17e11f5e724d0a7e918672c371d0c49d65c5cf
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 20:47:41 -0300

    10/06 Modificaciones en clase Lucha

--- Pokedexa/src/Pokemon/Lucha.java
+++ Pokedexa/src/Pokemon/Lucha.java
@@ -1,5 +1,46 @@
 package Pokemon;
 
-public class Lucha implements Lucha {
+public class Lucha extends Pokemon implements ILucha {
+	
+		/**
+	    * Constructor COPIA
+	    * @param id
+	    * @param nombre
+	    * @param vidas
+	    * @param genero
+	    * @param rutaImagen
+	    */
+	   public Lucha (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+	   {
+	   	super(id, nombre,nivel, vidas, genero, tipoLucha, rutaImagen);
+	   }
+	   
+	   /**
+	    * Constructor DEFECTO
+	    * @param id
+	    * @param nombre
+	    * @param genero
+	    * @param tipo
+	    * @param rutaImagen
+	    */
+	   public Lucha  (int id, String nombre, String genero, String tipo,String rutaImagen)
+	   {
+	   	super(id, nombre,genero, tipoLucha, rutaImagen);
+	   	setVidasNivel(calcularNivel());
+	   }
+	   
+	 
+		
+		/**
+		 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+		 */
+		protected int calcularNivel() 
+		{
+			return nivelLucha+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+		}
 
 }
+
+
+
+
