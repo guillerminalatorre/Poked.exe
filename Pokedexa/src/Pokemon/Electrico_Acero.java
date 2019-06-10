5daa7746bbaeda489b55e239cf76c6cc5de54a9c
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 20:54:05 -0300

    10/06 Modificaciones en clase Electrico_Acero

--- Pokedexa/src/Pokemon/Electrico_Acero.java
+++ Pokedexa/src/Pokemon/Electrico_Acero.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Electrico_Acero implements IElectrico , IAcero{
+public class Electrico_Acero extends Pokemon implements IElectrico , IAcero{
+	
+	/**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Electrico_Acero  (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoElectrico + tipoAcero, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Electrico_Acero (int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoElectrico + tipoAcero, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelElectrico+nivelAcero+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
