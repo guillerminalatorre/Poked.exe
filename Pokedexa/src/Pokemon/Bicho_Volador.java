fa411bd44ab65fc3ee6511e4fd5333336dbb0b10
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 20:56:29 -0300

    10/06 Modificaciones en clase Bicho_Volador

--- Pokedexa/src/Pokemon/Bicho_Volador.java
+++ Pokedexa/src/Pokemon/Bicho_Volador.java
@@ -1,5 +1,43 @@
 package Pokemon;
 
-public class Bicho_Volador implements IBicho, IVolador {
+public class Bicho_Volador extends Pokemon implements IBicho, IVolador {
+	
+	/**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Bicho_Volador (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoVolador + tipoBicho, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Bicho_Volador(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoVolador + tipoBicho, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelVolador+nivelBicho+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
+
