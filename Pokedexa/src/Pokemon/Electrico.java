2fe2f1ee66fd82360e9f63942a6512f5bde9e307
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 20:53:05 -0300

    10/06 Modificaciones en clase Electrico

--- Pokedexa/src/Pokemon/Electrico.java
+++ Pokedexa/src/Pokemon/Electrico.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Electrico implements IElectrico {
+public class Electrico extends Pokemon implements IElectrico {
+	
+	/**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Electrico (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoElectrico, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Electrico (int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoElectrico, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelElectrico+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
