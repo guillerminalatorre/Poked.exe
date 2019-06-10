1ee5f49250eeeba69e022546cecce70a7a329278
ILopez2 <48885606+ILopez2@users.noreply.github.com>
2019-06-10 12:25:56 -0300

    10/06 Modificaciones en clase Hielo_Volador
    Se crearon constructores

--- Pokedexa/src/Pokemon/Hielo_Volador.java
+++ Pokedexa/src/Pokemon/Hielo_Volador.java
@@ -1,5 +1,44 @@
 package Pokemon;
 
-public class Hielo_Volador implements IHielo , IVolador{
+public class Hielo_Volador extends Pokemon implements IHielo , IVolador{
+
+
+    
+    /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Hielo_Volador (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoHielo + tipoVolador, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Hielo_Volador (int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoHielo + tipoVolador, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelHielo+nivelVolador+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
