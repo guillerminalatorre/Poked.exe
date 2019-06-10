e6c510b372adcf28fc9c3cd3e4ac02119babaa05
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 20:50:20 -0300

    10/06 Modificaciones en clase Fuego_Volador

--- Pokedexa/src/Pokemon/Fuego_Volador.java
+++ Pokedexa/src/Pokemon/Fuego_Volador.java
@@ -1,5 +1,43 @@
 package Pokemon;
 
-public class Fuego_Volador implements IFuego, IVolador {
+public class Fuego_Volador extends Pokemon implements IFuego, IVolador {
+	
+		/**
+	    * Constructor COPIA
+	    * @param id
+	    * @param nombre
+	    * @param vidas
+	    * @param genero
+	    * @param rutaImagen
+	    */
+	   public Fuego_Volador  (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+	   {
+	   	super(id, nombre,nivel, vidas, genero, tipoFuego + tipoVolador, rutaImagen);
+	   }
+	   
+	   /**
+	    * Constructor DEFECTO
+	    * @param id
+	    * @param nombre
+	    * @param genero
+	    * @param tipo
+	    * @param rutaImagen
+	    */
+	   public Fuego_Volador  (int id, String nombre, String genero, String tipo,String rutaImagen)
+	   {
+	   	super(id, nombre,genero, tipoFuego + tipoVolador, rutaImagen);
+	   	setVidasNivel(calcularNivel());
+	   }
+	   
+	 
+		
+		/**
+		 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+		 */
+		protected int calcularNivel() 
+		{
+			return nivelFuego+nivelVolador+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+		}
 
 }
+
