4d675396a3129226f123a5497e728fbaa4fbee60
ILopez2 <48885606+ILopez2@users.noreply.github.com>
2019-06-10 12:23:49 -0300

    10/06 Modificaciones en clase Hielo_Psiquico
    Se crearon constructores

--- Pokedexa/src/Pokemon/Hielo_Psiquico.java
+++ Pokedexa/src/Pokemon/Hielo_Psiquico.java
@@ -1,5 +1,44 @@
 package Pokemon;
 
-public class Hielo_Psiquico implements IHielo , IPsiquico{
+public class Hielo_Psiquico extends Pokemon implements IHielo , IPsiquico{
+
+
+    
+    /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Hielo_Psiquico (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoHielo + tipoPsiquico, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Hielo_Psiquico(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoHielo + tipoPsiquico, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelHielo+nivelPsiquico+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
