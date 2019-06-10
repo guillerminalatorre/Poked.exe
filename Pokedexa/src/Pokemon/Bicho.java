6409b56a2519eb364cd0d7fcf2257d32f4dff155
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 20:58:37 -0300

    10/06 Modificaciones en clase Bicho

--- Pokedexa/src/Pokemon/Bicho.java
+++ Pokedexa/src/Pokemon/Bicho.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Bicho implements IBicho {
+public class Bicho extends Pokemon implements IBicho {
+    
+     /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Bicho (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoBicho, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Bicho(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero,tipoBicho, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelBicho+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
