08f339ff2b64015dbbe6542458afb08a20e9900b
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 20:59:35 -0300

    10/06 Modificaciones en clase Agua

--- Pokedexa/src/Pokemon/Agua.java
+++ Pokedexa/src/Pokemon/Agua.java
@@ -1,5 +1,43 @@
 package Pokemon;
 
-public class Agua implements IAgua {
+public class Agua extends Pokemon implements IAgua {
+
+
+    /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Agua (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoAgua, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Agua(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoAgua, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelAgua+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
