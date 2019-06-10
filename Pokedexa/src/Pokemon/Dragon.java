3dc9bf0a2520ecd7496aa61a551c2b38640e073c
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 20:55:56 -0300

    10/06 Modificaciones en clase Dragon

--- Pokedexa/src/Pokemon/Dragon.java
+++ Pokedexa/src/Pokemon/Dragon.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Dragon implements IDragon {
+public class Dragon extends Pokemon implements IDragon {
+	
+	/**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Dragon (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoDragon , rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Dragon(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoDragon , rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelDrago+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
