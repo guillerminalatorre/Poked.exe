3139ae4e84b127ccf4186d7094740c1326460a0c
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 20:57:02 -0300

    10/06 Modificaciones en clase Bicho_Veneno

--- Pokedexa/src/Pokemon/Bicho_Veneno.java
+++ Pokedexa/src/Pokemon/Bicho_Veneno.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Bicho_Veneno implements IBicho, IVeneno {
+public class Bicho_Veneno extends Pokemon implements IBicho, IVeneno {
+
+	/**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Bicho_Veneno (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoVeneno + tipoBicho, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Bicho_Veneno(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoVeneno + tipoBicho, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelVeneno+nivelBicho+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
