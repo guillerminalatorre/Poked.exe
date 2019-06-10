ed0e270254164cedd702666269af5f5069b9d3d3
ILopez2 <48885606+ILopez2@users.noreply.github.com>
2019-06-10 12:18:47 -0300

    10/06 Modificaciones en clase Fuego
    Se crearon constructores

--- Pokedexa/src/Pokemon/Fuego.java
+++ Pokedexa/src/Pokemon/Fuego.java
@@ -1,5 +1,44 @@
 package Pokemon;
 
-public class Fuego implements IFuego {
+public class Fuego extends Pokemon implements IFuego {
+
+
+    
+    /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Fuego (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoFuego, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Fuego(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoFuego, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelFuego+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
