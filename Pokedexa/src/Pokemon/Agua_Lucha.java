3bdcc09a022e589ceeea74bd1a1d857566607b57
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 21:00:23 -0300

    10/06 Modificaciones en clase Agua_Lucha

--- Pokedexa/src/Pokemon/Agua_Lucha.java
+++ Pokedexa/src/Pokemon/Agua_Lucha.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Agua_Lucha implements IAgua, ILucha {
+public class Agua_Lucha extends Pokemon implements IAgua, ILucha {
+	  
+    /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Agua_Lucha (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoAgua + tipoLucha, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Agua_Lucha (int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoAgua + tipoLucha, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelAgua+nivelLucha+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
