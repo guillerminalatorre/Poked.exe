f51d286f958f6dda71af744930dcc49f698605c3
matiara <49988089+matiara@users.noreply.github.com>
2019-06-11 16:49:44 -0300

    11/06 Modificaciones en clase Normal

--- Pokedexa/src/Pokemon/Normal.java
+++ Pokedexa/src/Pokemon/Normal.java
@@ -1,5 +1,43 @@
 package Pokemon;
 
-public class Normal implements INormal {
+public class Normal extends Pokemon implements INormal {
+
+/**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Normal (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoNormal, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Normal(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoNormal, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelNormal+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
+
