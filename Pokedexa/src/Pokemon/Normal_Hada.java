da8fc9ee179b7bccf2280a1e5ff86e98bdc65104
matiara <49988089+matiara@users.noreply.github.com>
2019-06-11 16:52:41 -0300

    11/06 Modificaciones en clase Normal_Hada

--- Pokedexa/src/Pokemon/Normal_Hada.java
+++ Pokedexa/src/Pokemon/Normal_Hada.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Normal_Hada implements INormal, IHada {
+public class Normal_Hada extends Pokemon implements INormal, IHada {
+
+    /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Normal_Hada (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoNormal + tipoHada, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Normal_Hada(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoNormal + tipoHada, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelNormal+nivelHada+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
