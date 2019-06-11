1b8132a673bb7563133769eb506805449c144225
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 21:03:22 -0300

    10/06 Modificaciones en clase Agua_Volador

--- Pokedexa/src/Pokemon/Agua_Volador.java
+++ Pokedexa/src/Pokemon/Agua_Volador.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Agua_Volador implements IAgua , IVolador{
+public class Agua_Volador extends Pokemon implements  IAgua , IVolador{
+
+    /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Agua_Volador (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoAgua+tipoVolador, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Agua_Volador(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoAgua + tipoVolador, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelAgua+nivelVolador+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
