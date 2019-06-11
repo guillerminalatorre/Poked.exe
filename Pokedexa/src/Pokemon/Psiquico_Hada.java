0fd6469dea7c7982e8147b4a476e2de3c683a5e0
matiara <49988089+matiara@users.noreply.github.com>
2019-06-11 17:03:54 -0300

    11/06 Modificaciones en clase Psiquico_Hada

--- Pokedexa/src/Pokemon/Psiquico_Hada.java
+++ Pokedexa/src/Pokemon/Psiquico_Hada.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Psiquico_Hada implements IPsiquico , IHada{
+public class Psiquico extends Pokemon implements  IPsiquico , IHada{
+	
+    /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Psiquico_Hada (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoPsiquico + tipoHada, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Psiquico_Hada(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoPsiquico + tipoHada, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelPsiquico+nivelHada+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
