bcc8f6611926285322665e6ceddcb65f480bb862
matiara <49988089+matiara@users.noreply.github.com>
2019-06-11 16:56:34 -0300

    11/06 Modificaciones en clase  Planta_Psiquico

--- Pokedexa/src/Pokemon/Planta_Psiquico.java
+++ Pokedexa/src/Pokemon/Planta_Psiquico.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Planta_Psiquico implements IPlanta , IPsiquico{
+public class Planta_Psiquico extends Pokemon implements IPlanta , IPsiquico{
+	
+    /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Planta_Psiquico (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoPlanta + tipoPsiquico, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Planta_Psiquico(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoPlanta + tipoPsiquico, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelPlanta+nivelPsiquico+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
