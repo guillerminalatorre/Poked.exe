48ae3bde0e42ecec141a9023ae0f1231482d6906
matiara <49988089+matiara@users.noreply.github.com>
2019-06-11 16:57:40 -0300

    11/06 Modificaciones en clase Psiquico

--- Pokedexa/src/Pokemon/Psiquico.java
+++ Pokedexa/src/Pokemon/Psiquico.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Psiquico implements IPsiquico {
+public class Psiquico extends Pokemon implements  IPsiquico{
+	
+    /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Psiquico (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoPsiquico, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Psiquico(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoPsiquico, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelPsiquico+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
