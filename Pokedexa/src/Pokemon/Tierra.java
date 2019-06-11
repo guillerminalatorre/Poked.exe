2a8fba1b70df0c8a7e7e9b1648c79310187246f1
matiara <49988089+matiara@users.noreply.github.com>
2019-06-11 17:08:09 -0300

    11/06 Modificaciones en clase  Tierra

--- Pokedexa/src/Pokemon/Tierra.java
+++ Pokedexa/src/Pokemon/Tierra.java
@@ -1,5 +1,43 @@
 package Pokemon;
 
-public class Tierra implements ITierra {
+public class Tierra extends Pokemon implements ITierra{
 
+
+	/**
+	 * Constructor COPIA
+	 * @param id
+	 * @param nombre
+	 * @param vidas
+	 * @param genero
+	 * @param rutaImagen
+	 */
+	public Tierra (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+	{
+		super(id, nombre,nivel, vidas, genero,  tipoTierra, rutaImagen);
+	}
+	
+	/**
+	 * Constructor DEFECTO
+	 * @param id
+	 * @param nombre
+	 * @param genero
+	 * @param tipo
+	 * @param rutaImagen
+	 */
+	public Tierra(int id, String nombre, String genero, String tipo,String rutaImagen)
+	{
+		super(id, nombre,genero,  tipoTierra, rutaImagen);
+		setVidasNivel(calcularNivel());
+	}
+	
+	
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelTierra+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
+	
 }
