7b608a85dd89b00a4d0062d6ac95e25a047b311d
matiara <49988089+matiara@users.noreply.github.com>
2019-06-11 16:54:25 -0300

    11/06 Modificaciones en clase Normal_Volador

--- Pokedexa/src/Pokemon/Normal_Volador.java
+++ Pokedexa/src/Pokemon/Normal_Volador.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Normal_Volador implements INormal, IVolador {
+public class Normal_Volador extends Pokemon implements INormal, IVolador {
+	
+	/**
+    * Constructor COPIA
+    * @param id
+    * @param nombre
+    * @param vidas
+    * @param genero
+    * @param rutaImagen
+    */
+   public Normal_Volador (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+   {
+   	super(id, nombre,nivel, vidas, genero, tipoNormal + tipoVolador, rutaImagen);
+   }
+   
+   /**
+    * Constructor DEFECTO
+    * @param id
+    * @param nombre
+    * @param genero
+    * @param tipo
+    * @param rutaImagen
+    */
+   public Normal_Volador(int id, String nombre, String genero, String tipo,String rutaImagen)
+   {
+   	super(id, nombre,genero, tipoNormal + tipoVolador, rutaImagen);
+   	setVidasNivel(calcularNivel());
+   }
+   
+ 
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelNormal+nivelVolador+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
