501057136c0f424391d0b31ee8c28f743963aa6f
matiara <49988089+matiara@users.noreply.github.com>
2019-06-10 21:02:14 -0300

    10/06 Modificaciones en clase Agua_Veneno

--- Pokedexa/src/Pokemon/Agua_Veneno.java
+++ Pokedexa/src/Pokemon/Agua_Veneno.java
@@ -1,5 +1,43 @@
 package Pokemon;
 
-public class Agua_Veneno implements IAgua , IVeneno{
+public class Agua_Veneno extends Pokemon implements IAgua , IVeneno{
+
+	 
+    /**
+     * Constructor COPIA
+     * @param id
+     * @param nombre
+     * @param vidas
+     * @param genero
+     * @param rutaImagen
+     */
+    public Agua_Veneno (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+    {
+    	super(id, nombre,nivel, vidas, genero, tipoAgua + tipoVeneno, rutaImagen);
+    }
+    
+    /**
+     * Constructor DEFECTO
+     * @param id
+     * @param nombre
+     * @param genero
+     * @param tipo
+     * @param rutaImagen
+     */
+    public Agua_Veneno(int id, String nombre, String genero, String tipo,String rutaImagen)
+    {
+    	super(id, nombre,genero, tipoAgua + tipoVeneno, rutaImagen);
+    	setVidasNivel(calcularNivel());
+    }
+    
+  
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelAgua+nivelVeneno+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
 
 }
