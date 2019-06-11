3dd982b4c1628c90e725fa3219d27c4708934112
matiara <49988089+matiara@users.noreply.github.com>
2019-06-11 17:09:55 -0300

    11/06 Modificaciones en clase Tierra_Veneno

--- Pokedexa/src/Pokemon/Tierra_Veneno.java
+++ Pokedexa/src/Pokemon/Tierra_Veneno.java
@@ -1,5 +1,42 @@
 package Pokemon;
 
-public class Tierra_Veneno implements ITierra , IVeneno {
+public class Tierra_Veneno extends Pokemon implements ITierra , IVeneno {
 
+	/**
+	 * Constructor COPIA
+	 * @param id
+	 * @param nombre
+	 * @param vidas
+	 * @param genero
+	 * @param rutaImagen
+	 */
+	public Tierra_Veneno (int id, String nombre,int nivel, int vidas, String genero,String rutaImagen)
+	{
+		super(id, nombre,nivel, vidas, genero,  tipoTierra + tipoVeneno, rutaImagen);
+	}
+	
+	/**
+	 * Constructor DEFECTO
+	 * @param id
+	 * @param nombre
+	 * @param genero
+	 * @param tipo
+	 * @param rutaImagen
+	 */
+	public Tierra_Veneno (int id, String nombre, String genero, String tipo,String rutaImagen)
+	{
+		super(id, nombre,genero,  tipoTierra + tipoVeneno, rutaImagen);
+		setVidasNivel(calcularNivel());
+	}
+	
+	
+	
+	/**
+	 * Metodo abstracto entre las clases solo cambia el "tipo" las evoluciones quedan igual.
+	 */
+	protected int calcularNivel() 
+	{
+		return nivelTierra+nivelVeneno+nivelPrimeraEvolucion()+nivelSegundaEvolucion()+nivelTerceraEvolucion();
+	}
+	
 }
