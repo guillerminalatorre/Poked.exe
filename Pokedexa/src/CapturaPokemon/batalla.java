335acecd0e4525238a394f0f0de29a0a649d54ad
matiara <49988089+matiara@users.noreply.github.com>
2019-06-13 12:31:42 -0300

    13/06 clase batalla

--- Pokedexa/src/CapturaPokemon/batalla.java
+++ Pokedexa/src/CapturaPokemon/batalla.java
@@ -1,42 +1,75 @@
 package CapturaPokemon;
 
+import Pokemon.Pokemon;
+
 public class batalla {
   
-	private String Pokemon_Capturado;
-	private String Pokemon_Salvaje;
+	private Pokemon Pokemon_Capturado;
+	private Pokemon Pokemon_Salvaje;
 	private int Ganador;
 	
-	public batalla(String Pokemon_Capturado,String Pokemon_Salvaje){
+	public batalla(Pokemon Pokemon_Capturado ,Pokemon Pokemon_Salvaje ){
 		
 		this.Pokemon_Capturado = Pokemon_Capturado;
-		this.Pokemon_Salvaje = Pokemon_Capturado;
+		this.Pokemon_Salvaje = Pokemon_Salvaje;
 		setGanador(0);
 		
-		
-	}
+		}
 
-	public String getPokemon_Salvaje() {
-		return Pokemon_Salvaje;
-	}
-
-	public void setPokemon_Salvaje(String pokemon_Salvaje) {
-		Pokemon_Salvaje = pokemon_Salvaje;
-	}
 
 	public int getGanador() {
 		return Ganador;
 	}
 
-	public void setGanador(int ganador) {
-		Ganador = ganador;
+	public void setGanador(double d) {
+		Ganador = (int) d;
 	}
 
-	public String getPokemon_Capturado() {
+
+	public Pokemon getPokemon_Salvaje() {
+		return Pokemon_Salvaje;
+	}
+
+
+	public void setPokemon_Salvaje(Pokemon pokemon_Salvaje) {
+		Pokemon_Salvaje = pokemon_Salvaje;
+	}
+
+
+	public Pokemon getPokemon_Capturado() {
 		return Pokemon_Capturado;
 	}
+
 	
-	private int calcularGanador(){
-
+	public int calcularGanador(){
+		
+		int nivel_ganador = 0;
+		
+		if(Pokemon_Capturado instanceof Pokemon ) {
+		}	
+			else if(Pokemon_Salvaje instanceof Pokemon ) {
+				
+				nivel_ganador = Pokemon_Capturado.getNivel() - Pokemon_Salvaje.getNivel();
+			}
+	
+		return nivel_ganador;		
 	}
-
-}
+	
+	public void definirGanador() {
+		
+		int x = calcularGanador();
+		
+		if(x > 0) {
+			setGanador(1);
+		}
+		else  if (x < 0 ) {
+			setGanador(x);
+		}
+		else  {
+			setGanador(Math.random()*3 + 1);
+		}	
+		
+	}
+	
+}	
+	
