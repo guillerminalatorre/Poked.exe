838db735afa3d8971b5472d301a7cf80220242ed
guillerminalatorre <44948061+guillerminalatorre@users.noreply.github.com>
2019-06-23 18:53:05 -0300

    23/6 Cambios clase Pokemon
    eliminacion de atributo genero
    nuevo atributo evolucionn
    cambio del cuerpo de nivelPrimeraEvolucion(), nivelSegundaEvolucion(), nivelTerceraEvolucion().

--- Pokedexa/src/Pokemon/Pokemon.java
+++ Pokedexa/src/Pokemon/Pokemon.java
@@ -6,7 +6,7 @@ public abstract class Pokemon
 	private String nombre;
 	private int vidas;
 	private int nivel;
-	private String genero;
+	private int evolucion;
 	private String tipo;
 	private String rutaImagen;
 	
@@ -18,15 +18,15 @@ public abstract class Pokemon
 	 * @param nombre
 	 * @param vidas
 	 * @param nivel
-	 * @param genero
+	 * @param evolucion
 	 */
-	public Pokemon (int id, String nombre,int Nivel,  int Vidas, String genero, String tipo,String rutaImagen)
+	public Pokemon (int id, String nombre,int Nivel,  int Vidas, int evolucion, String tipo,String rutaImagen)
 	{
 		this.id = id;
 		this.nombre = nombre;
 		setVidas(vidas);
 		setNivel(nivel);
-		this.genero = genero;
+		this.evolucion = evolucion;
 		this.tipo = tipo;
 		this.rutaImagen = rutaImagen;
 	}
@@ -36,15 +36,15 @@ public abstract class Pokemon
 	 * @param id
 	 * @param nombre
 	 * @param nivel
-	 * @param genero
+	 * @param evolucion
 	 */
-	public Pokemon (int id, String nombre, String genero, String tipo,String rutaImagen)
+	public Pokemon (int id, String nombre, int evolucion, String tipo,String rutaImagen)
 	{
 		this.id = id;
 		this.nombre = nombre;
 		setVidas(0);
 		setNivel(0);
-		this.genero = genero;
+		this.evolucion = evolucion;
 		this.rutaImagen = rutaImagen;
 	}
 	
@@ -77,8 +77,8 @@ public abstract class Pokemon
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public void setGenero(String genero) {
-		this.genero = genero;
+	public void setEvolucion(int evolucion) {
+		this.evolucion = evolucion;
 	}
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
@@ -104,8 +104,8 @@ public abstract class Pokemon
 		return nivel;
 	}
 
-	public String getGenero() {
-		return genero;
+	public int getEvolucion() {
+		return evolucion;
 	}
 	
 	public String getTipo() {
@@ -119,7 +119,7 @@ public abstract class Pokemon
 	
 	protected int nivelPrimeraEvolucion()
 	{	
-		int nivel = 0;
+		/*int nivel = 0;
 		int[] primeraEvolucion = {1,4,7,10,13,16,19,21,23,25,27,29,32,35,37,39,41,43,46,48,50,52,54,56,58,60,63,66,69,72,74,77,79,81,83,84,86,88,90,92,95,96,98,100,102,104,106,108,109,111,113,114,115,116,118,120,122,123,124,125,126,127,128,129,131,132,133,140};
 		int recorre=0;
 		while(nivel == 0)
@@ -127,12 +127,15 @@ public abstract class Pokemon
 			if(primeraEvolucion[recorre] == getId()) nivel=1;
 			recorre++;
 		}
+		return nivel;*/
+		int nivel = 0;
+		if (getEvolucion() == 1 ) nivel =1;
 		return nivel;
 	}
 	
 	protected int nivelSegundaEvolucion()
 	{
-		int nivel = 0;
+		/*int nivel = 0;
 		int[] segundaEvolucion = {2,5,8,11,14,17,20,22,24,26,28,30,33,36,38,40,42,44,47,49,51,53,55,57,59,61,64,67,70,73,75,78,80,82,85,87,89,91,93,97,99,101,103,105,107,110,112,117,119,121,134,135,146,137,139,141};
 		int recorre=0;
 		while(nivel == 0)
@@ -140,12 +143,15 @@ public abstract class Pokemon
 			if(segundaEvolucion[recorre] == getId()) nivel=10;
 			recorre++;
 		}
+		return nivel;*/
+		int nivel = 0;
+		if (getEvolucion() == 2 ) nivel =10;
 		return nivel;
 	}
 	
 	protected int nivelTerceraEvolucion ()
 	{
-		int nivel = 0;
+		/*int nivel = 0;
 		int[] terceraEvolucion = {3,6,9,12,15,18,31,34,45,62,65,68,71,76,94,130,136,138,142,143,144,145,147,148,149,150};
 		int recorre=0;
 		while(nivel == 0)
@@ -153,6 +159,9 @@ public abstract class Pokemon
 			if(terceraEvolucion[recorre] == getId()) nivel=20;
 			recorre++;
 		}
+		return nivel;*/
+		int nivel = 0;
+		if (getEvolucion() == 1 ) nivel =20;
 		return nivel;
 	}
 	
@@ -160,7 +169,7 @@ public abstract class Pokemon
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+		result = prime * result + evolucion;
 		result = prime * result + id;
 		result = prime * result + nivel;
 		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
@@ -197,7 +206,7 @@ public abstract class Pokemon
 	@Override
 	public String toString() {
 		return "Pokemon [id=" + id + ", nombre=" + nombre + ", vidas=" + vidas + ", nivel=" + nivel + ", genero="
-				+ genero + "]";
+				+ evolucion + "]";
 	}
 	
 }
