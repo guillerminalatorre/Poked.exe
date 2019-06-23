e4b7ec84a7e642115565368a988d3102cd5ea19a
guillerminalatorre <44948061+guillerminalatorre@users.noreply.github.com>
2019-06-23 20:23:38 -0300

    23/6 Cambios en Batalla

--- Pokedexa/src/ManejadorExcepciones/ExcepcionGenerica.java
+++ Pokedexa/src/ManejadorExcepciones/ExcepcionGenerica.java
@@ -4,12 +4,15 @@ package ManejadorExcepciones;
  * Clase generica para el manejo de excpeciones 
  */
 @SuppressWarnings("serial")
-public class ExcepcionGenerica extends Exception{
+public class ExcepcionGenerica extends Exception
+{
 	String informacion;
+	
 	public ExcepcionGenerica (String info) {
 		super(info);
 		informacion=info;
 	}
+	
 	public String MensajeError() {
 		return informacion;
 	}
