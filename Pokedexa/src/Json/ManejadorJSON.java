68601c76d22278befa73799749515791e11ae722
ILopez2 <48885606+ILopez2@users.noreply.github.com>
2019-06-23 13:04:34 -0300

    23/06 Grandes cambios leer descripcion
    Se modifico parte del treemap de usuarios, se modifico metodos de clase pokemon, se creeo una excepcion generica que tiene un metodo que retorna un string con el error generado y este lo tienen que impimir por pantalla ( leer por favor). Se creo un manejador de JSON que sirve para cargar pokemones y tambien te los retorna.

--- Pokedexa/src/Json/ManejadorJSON.java
+++ Pokedexa/src/Json/ManejadorJSON.java
@@ -1,20 +1,60 @@
+package Json;
 import org.json.*;
+import Pokemon.*;
+import ManejadorExcepciones.*;
 
 public class ManejadorJSON {
 	JSONArray pokemones;
-	JSONObject pokemon;
-	Pokemon pokemonObj;
+	JSONObject pokemonJSON;
+	Pokemon pokemon=null;
+	//Pokemon pokemonObj;
 	
-	public ManejadorJSON() throws ErrorGenerico {
+	public ManejadorJSON() throws ExcepcionGenerica {
 		try {
 			pokemones=new JSONArray("pokemones");
 		}
 		catch(JSONException ErrorJson) {
 			ErrorJson.printStackTrace();
-			throw new ErrorGenerico("Error al realizar al crear el JSONArray");
+			throw new ExcepcionGenerica("Error al realizar al crear el JSONArray");
 		}
 	}
-	public void CargarPokemon(int id, String nombre , int vidas , int nivel , String genero , String tipo , String rutaImagen) {
-		
+	public void CargarPokemon(Pokemon pokemonObj) throws ExcepcionGenerica{
+		try {
+			pokemonJSON.put("Id", pokemonObj.getId());
+			pokemonJSON.put("Nombre", pokemonObj.getNombre());
+			pokemonJSON.put("Nivel", pokemonObj.getNivel());
+			pokemonJSON.put("Vidas", pokemonObj.getVidas());
+			pokemonJSON.put("Genero", pokemonObj.getGenero());
+			pokemonJSON.put("Tipo", pokemonObj.getTipo());
+			pokemonJSON.put("RutaImagen", pokemonObj.getRutaImagen());
+			pokemones.put(pokemonJSON);
+		}
+		catch(JSONException ErrorJSON) {
+			ErrorJSON.printStackTrace();
+			throw new ExcepcionGenerica("Error al cargar pokemon"+pokemonObj.getNombre()+" en archivo JSON");
+		}
 	}
+	
+	@SuppressWarnings("finally")
+	public Pokemon SacarPokemonJSON(int id) throws ExcepcionGenerica{
+
+		try {
+			pokemonJSON=pokemones.getJSONObject(id);
+			pokemon.setId(pokemonJSON.getInt("Id"));
+			pokemon.setNombre(pokemonJSON.getString("Nombre"));
+			pokemon.setNivel(pokemonJSON.getInt("Nivel"));
+			pokemon.setVidas(pokemonJSON.getInt("Vidas"));
+			pokemon.setGenero(pokemonJSON.getString("Genero"));
+			pokemon.setTipo(pokemonJSON.getString("Tipo"));
+			pokemon.setRutaImagen(pokemonJSON.getString("RutaImagen"));
+		}
+			catch(JSONException ErrorJSON) {
+				ErrorJSON.printStackTrace();
+				throw new ExcepcionGenerica("Error al abrir el archivo JSON");
+			}
+		finally {
+			return pokemon;
+		}
+	}
+	
 }
