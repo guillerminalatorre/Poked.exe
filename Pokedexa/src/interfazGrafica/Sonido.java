070af3766a166a663e3cc1d1141fc1c1993878dc
ILopez2 <48885606+ILopez2@users.noreply.github.com>
2019-06-23 21:33:15 -0300

    23/06 Parte grafica
    Se carga interfaz grafica de la ventana de logueo y un objeto reproductor para los sonidos.

---
+++ Pokedexa/src/interfazGrafica/Sonido.java
@@ -0,0 +1,45 @@
+package interfazGrafica;
+import javazoom.jlme.util.*;
+import java.io.*;
+import ManejadorExcepciones.ExcepcionGenerica;
+
+
+public class Sonido {
+	private Player Reproductor;
+	File Archivo;
+	public Sonido(String ruta) throws ExcepcionGenerica{
+		Archivo=new File(ruta);
+		try {
+			Reproductor= new Player(new FileInputStream(Archivo));
+		}
+		catch(FileNotFoundException error) {
+			error.printStackTrace();
+			throw new ExcepcionGenerica("Error al abrir el archivo");
+		}
+		catch(Exception error) {
+			error.printStackTrace();
+			throw new ExcepcionGenerica("Error al abrir el reproductor de sonidos");
+		}
+	}
+	
+	public void Reproducir() throws ExcepcionGenerica{
+		try {
+			Reproductor.play();
+		}
+		catch(Exception error) {
+			error.printStackTrace();
+			throw new ExcepcionGenerica("Error al reproducir sonido");
+		}
+	}
+	public void Parar() throws ExcepcionGenerica{
+		try {
+			Reproductor.stop();
+		}
+		catch(Exception error) {
+			error.printStackTrace();
+			throw new ExcepcionGenerica("Error al parar sonido");
+		
+		}
+	}
+}
+	
