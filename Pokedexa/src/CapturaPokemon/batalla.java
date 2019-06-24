c0f73847a6fc645269b8a4814f054580d51d0631
guillerminalatorre <44948061+guillerminalatorre@users.noreply.github.com>
2019-06-23 22:24:08 -0300

    23/6 Clase Batalla

--- Pokedexa/src/CapturaPokemon/batalla.java
+++ Pokedexa/src/CapturaPokemon/batalla.java
@@ -1,18 +1,22 @@
 package CapturaPokemon;
 
+import ManejadorExcepciones.ExcepcionGenerica;
 import Pokemon.*;
+import Usuario.*;
 
 public class Batalla {
   
+	private Usuario usuario;
 	private Pokemon pokemonCapturado;
 	private Pokemon pokemonSalvaje;
 	private int Ganador;
 	
-	public Batalla(Pokemon pokemonCapturado ,Pokemon pokemonSalvaje )
+	public Batalla(Pokemon pokemonCapturado ,Pokemon pokemonSalvaje, Usuario usuario)
 	{
 		this.pokemonCapturado = pokemonCapturado;
 		this.pokemonSalvaje = pokemonSalvaje;
 		setGanador(0);
+		this.usuario = usuario;
 	}
 
 
@@ -76,7 +80,6 @@ public class Batalla {
 		int x = calcularGanador();
 		
 		if(x > 0) {
-			//pokemonCapturado.setNivel( pokemonCapturado.getNivel() + x);
 			setGanador(1);
 		}
 		else  if (x < 0 ) {
@@ -117,11 +120,135 @@ public class Batalla {
 		}
 	}
 	
-	public Pokemon nuevoPokemonCapturado ( Pokemon pokemonSalvaje )
+	/**
+	 * Si el pokemon no fue visto por el usuario lo carga.
+	 * @param pokemon
+	 * @throws ExcepcionGenerica
+	 */
+	public void agregarNuevoPokemonPokedex ( Pokemon pokemon ) throws ExcepcionGenerica
 	{
-		
+		if(! usuario.ElPokemonFueVisto(pokemon))
+		{
+			usuario.cargarNuevoPokemonVisto(pokemon);
+		}
 	}
 	
 	
-}	
-	
+	/**
+	 * método para averigüar a que subclase de Pokemon pertenece el pokemon Salvaje y guardarlo en el archivo binario capturados
+	 * @param pokemon
+	 * @return
+	 */
+	public void agregarNuevoPokemonCapturado ( Pokemon pokemon ) throws ExcepcionGenerica
+	{
+
+		if (pokemon instanceof Agua_Hielo) {
+			usuario.cargarNuevoPokemonCapturado(new Agua_Hielo(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Agua_Lucha) {
+			usuario.cargarNuevoPokemonCapturado(new Agua_Lucha(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Agua_Psiquico) {
+			usuario.cargarNuevoPokemonCapturado(new Agua_Psiquico(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Agua_Veneno) {
+			usuario.cargarNuevoPokemonCapturado(new Agua_Veneno(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Agua_Volador) {
+			usuario.cargarNuevoPokemonCapturado(new Agua_Volador(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Agua) {
+			usuario.cargarNuevoPokemonCapturado(new Agua(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Bicho_Planta) {
+			usuario.cargarNuevoPokemonCapturado(new Bicho_Veneno(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Bicho_Veneno) {
+			usuario.cargarNuevoPokemonCapturado(new Bicho_Veneno(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Bicho_Volador) {
+			usuario.cargarNuevoPokemonCapturado(new Bicho_Volador(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Bicho ) {
+			usuario.cargarNuevoPokemonCapturado(new Bicho(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Dragon_Volador) {
+			usuario.cargarNuevoPokemonCapturado(new Dragon_Volador(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Dragon) {
+			usuario.cargarNuevoPokemonCapturado(new Dragon(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Electrico_Acero) {
+			usuario.cargarNuevoPokemonCapturado(new Electrico_Volador(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Electrico_Volador) {
+			usuario.cargarNuevoPokemonCapturado(new Electrico(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Electrico) {
+			usuario.cargarNuevoPokemonCapturado(new Electrico(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Fantasma_Veneno) {
+			usuario.cargarNuevoPokemonCapturado(new Fantasma_Veneno(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Fuego_Volador) {
+			usuario.cargarNuevoPokemonCapturado(new Fuego_Volador(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		}
+		if (pokemon instanceof Fuego) {
+			usuario.cargarNuevoPokemonCapturado(new Fuego(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Hada) {
+			usuario.cargarNuevoPokemonCapturado(new Hada(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Hielo_Psiquico) {
+			usuario.cargarNuevoPokemonCapturado(new Hielo_Psiquico(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Hielo_Volador) {
+			usuario.cargarNuevoPokemonCapturado(new Hielo_Volador(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Lucha) {
+			usuario.cargarNuevoPokemonCapturado(new Lucha(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Normal_Hada) {
+			usuario.cargarNuevoPokemonCapturado(new Normal_Hada(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Normal_Volador) {
+			usuario.cargarNuevoPokemonCapturado(new Normal_Volador(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Normal) {
+			usuario.cargarNuevoPokemonCapturado(new Normal(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Planta_Psiquico) {
+			usuario.cargarNuevoPokemonCapturado(new Planta_Psiquico(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof PlantaVeneno) {
+			usuario.cargarNuevoPokemonCapturado(new PlantaVeneno(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Psiquico_Hada) {
+			usuario.cargarNuevoPokemonCapturado(new Psiquico_Hada(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Psiquico) {
+			usuario.cargarNuevoPokemonCapturado(new Psiquico(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Roca_Agua) {
+			usuario.cargarNuevoPokemonCapturado(new Roca_Agua(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Roca_Tierra) {
+			usuario.cargarNuevoPokemonCapturado(new Roca_Tierra(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Roca_Volador) {
+			usuario.cargarNuevoPokemonCapturado(new Roca_Volador(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Tierra_Veneno) {
+			usuario.cargarNuevoPokemonCapturado(new Tierra_Veneno(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Tierra) {
+			usuario.cargarNuevoPokemonCapturado(new Tierra(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Veneno_Volador) {
+			usuario.cargarNuevoPokemonCapturado(new Veneno_Volador(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+		if (pokemon instanceof Veneno) {
+			usuario.cargarNuevoPokemonCapturado(new Veneno(pokemon.getId(), pokemon.getNombre(), pokemon.getEvolucion(), pokemon.getTipo(), pokemon.getRutaImagen()));
+		} 
+	}
+}
